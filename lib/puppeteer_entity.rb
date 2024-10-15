121177e84ae79c0b5bb8bc0562302f2e262b056f
asgerb <a@asgerbehnckejacobsen.dk>
2024-10-15 16:26:45 +0200

    chore: gem scaffold

---
+++ lib/puppeteer_entity.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "puppeteer_entity/version"
+
+module PuppeteerEntity
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/puppeteer_entity/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PuppeteerEntity
+  VERSION = "0.1.0"
+end
