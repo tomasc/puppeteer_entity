d7c08f545d85ab3bd35b6e59eb30cc434b8a51ec
asgerb <a@asgerbehnckejacobsen.dk>
2024-10-16 11:27:29 +0200

    chore: content test

--- test/puppeteer_entity/content_test.rb
+++ test/puppeteer_entity/content_test.rb
@@ -3,20 +3,14 @@ require "test_helper"
 
 class PuppeteerEntity::ContentTest < Minitest::Test
   def test_content
-    skip
     args = { url: "https://www.example.com" }
     entity = PuppeteerEntity::Content.new(args)
 
-    entity.file.refresh_metadata!
-    entity.refresh_metadata!
+    response = entity.response
 
-    assert_equal %w[filename height mime_type size width], entity.file.metadata.keys.sort
-    assert_predicate entity.file.metadata["filename"], :present?
-    assert_equal "text/html", entity.file.metadata["mime_type"]
-    assert_nil entity.file.metadata["width"]
-    assert_nil entity.file.metadata["height"]
-    assert_operator entity.file.metadata["size"], :>, 1_000
-    assert_equal "text/html", entity.file.mime_type
-    assert_operator entity.file.size, :>, 0
+    assert_instance_of HTTP::Response, response
+    assert_equal 200, response.status
+    assert_equal "text/html", response.content_type.mime_type
+    assert_includes response.body.to_s, "This domain is for use in illustrative examples in documents."
   end
 end
