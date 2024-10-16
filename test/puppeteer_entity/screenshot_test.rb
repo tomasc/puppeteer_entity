caa23d76fdd309b3b2a78088b9d36637e5233eaa
asgerb <a@asgerbehnckejacobsen.dk>
2024-10-16 11:25:24 +0200

    chore: screenshot test

--- test/puppeteer_entity/screenshot_test.rb
+++ test/puppeteer_entity/screenshot_test.rb
@@ -2,77 +2,33 @@
 require "test_helper"
 
 class PuppeteerEntity::ScreenshotTest < Minitest::Test
-  def test_screenshot_filename
-    skip
-    args = { url: Faker::Internet.url }
-    entity = PuppeteerEntity::Screenshot.new(args)
-    filename = entity.filename
-
-    assert_predicate filename, :present?
-  end
-
-  def test_screenshot_id
-    skip
-    url = "https://now-staging.gsd.harvard.edu/calendar/academic-calendar?filters%5Ball_academic_calendar%5D=0&filters%5Ball_academic_calendar%5D=1&filters%5Bacademic_calendar%5D%5B%5D=&filters%5Ball_events%5D=0&filters%5Ball_events%5D=1&filters%5Bevents%5D%5B%5D=&expanded%5Breviews%5D=1&filters%5Ball_reviews%5D=0&filters%5Breviews%5D%5B%5D=&filters%5Ball_affiliations%5D=0&filters%5Ball_affiliations%5D=1&filters%5Baffiliations%5D%5Bevent_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Barch_prog_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Blarch_prog_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Bupd_prog_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Bother_prog_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Bevent_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Boffice_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Bevent_affiliations%5D%5B%5D=&filters%5Baffiliations%5D%5Bstudent_affiliations%5D%5B%5D=&filters%5Ball_audiences%5D=0&filters%5Ball_audiences%5D=1&filters%5Baudiences%5D%5B%5D="
-    args = { url: url }
-    entity = PuppeteerEntity::Screenshot.new(args)
-    filename = entity.filename
-    id = entity.id
-
-    assert_predicate id, :end_with?, filename
-    assert_equal({ browserless_url: ENV["BROWSERLESS_URL"], retry_limit: 3, url: url }, PuppeteerEntity::Screenshot.from_id(id).to_h)
-  end
-
-  def test_screenshot_derivation_url
-    skip
-    args = { url: Faker::Internet.url }
+  def test_screenshot
+    args = { url: "https://example.com" }
     entity = PuppeteerEntity::Screenshot.new(args)
 
-    assert_respond_to entity, :derivation_url
-  end
+    response = entity.response
 
-  def test_screenshot_download_url
-    skip "Not implemented"
-    custom_filename = File.basename(Faker::File.file_name)
-    args = { url: Faker::Internet.url }
-    entity = PuppeteerEntity::Screenshot.new(args)
-
-    assert_includes entity.download_url(filename: custom_filename), custom_filename
-  end
-
-  def test_png_screenshot
-    skip
-    args = { url: "https://www.example.com", options: { type: :png } }
-    entity = PuppeteerEntity::Screenshot.new(args)
-
-    entity.file.refresh_metadata!
-    entity.refresh_metadata!
-
-    assert_equal %w[filename height mime_type size width], entity.file.metadata.keys.sort
-    assert_predicate entity.file.metadata["filename"], :present?
-    assert_equal "image/png", entity.file.metadata["mime_type"]
-    assert_equal 800, entity.file.metadata["width"]
-    assert_equal 600, entity.file.metadata["height"]
-    assert_operator entity.file.metadata["size"], :>, 1_000
-    assert_equal "image/png", entity.file.mime_type
-    assert_operator entity.file.size, :>, 0
+    assert_instance_of HTTP::Response, response
+    assert_equal 200, response.status
+    assert_equal "image/png", response.content_type.mime_type
+    refute_empty response.body.to_s
+    body_string = response.body.to_s
+    assert_equal "\x89PNG\r\n\x1A\n".b, body_string[0..7]
+    assert body_string.bytesize.between?(1000, 10_000_000)
   end
 
   def test_jpeg_screenshot
-    skip
     args = { url: "https://www.example.com", options: { type: :jpeg, quality: 80 } }
     entity = PuppeteerEntity::Screenshot.new(args)
 
-    entity.file.refresh_metadata!
-    entity.refresh_metadata!
+    response = entity.response
 
-    assert_equal %w[filename height mime_type size width], entity.file.metadata.keys.sort
-    assert_predicate entity.file.metadata["filename"], :present?
-    assert_equal "image/jpeg", entity.file.metadata["mime_type"]
-    assert_equal 800, entity.file.metadata["width"]
-    assert_equal 600, entity.file.metadata["height"]
-    assert_operator entity.file.metadata["size"], :>, 1_000
-    assert_equal "image/jpeg", entity.file.mime_type
-    assert_operator entity.file.size, :>, 0
+    assert_instance_of HTTP::Response, response
+    assert_equal 200, response.status
+    assert_equal "image/jpeg", response.content_type.mime_type
+    refute_empty response.body.to_s
+    body_string = response.body.to_s
+    assert_equal "\xFF\xD8\xFF\xE0".b, body_string[0..3]
+    assert body_string.bytesize.between?(1000, 10_000_000)
   end
 end
