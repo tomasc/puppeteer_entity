fdd01286b14a26070fb84d9f26868894e409112c
asgerb <a@asgerbehnckejacobsen.dk>
2024-10-16 09:57:37 +0200

    chore: dependencies

--- lib/puppeteer_entity.rb
+++ lib/puppeteer_entity.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
-require_relative "puppeteer_entity/version"
+require "active_support/gzip"
+require "dry/initializer"
+require "dry/transformer"
+require "http"
+require "universalid"
+
+require "zeitwerk"
+loader = Zeitwerk::Loader.new
+loader.tag = File.basename(__FILE__, ".rb")
+loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
+loader.push_dir(File.dirname(__FILE__))
 
 module PuppeteerEntity
   class Error < StandardError; end
-  # Your code goes here...
+
+  class RequestError < Error; end
+
+  class IdLengthError < Error; end
 end
